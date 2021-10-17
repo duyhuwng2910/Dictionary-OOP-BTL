db9f5186a5718d457c1d0d7a00ffd65bed154dd6
Nguyễn Duy Hưng <39574867+duyhuwng2910@users.noreply.github.com>
2021-10-17 18:06:36 +0700

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.example.btl_dictionary_graphic {
-  requires javafx.controls;
-  requires javafx.fxml;
-
-  opens com.example.btl_dictionary_graphic to javafx.fxml;
-  exports com.example.btl_dictionary_graphic;
-}
